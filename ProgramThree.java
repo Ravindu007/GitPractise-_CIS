43341cef5efbdec2c5d762020c520b5fdee274a3
Ravindu007 <ravindu0504@gmail.com>
2021-06-20 20:50:15 +0530

    added basic java file

---
+++ ProgramThree.java
@@ -0,0 +1,17 @@
+public class ProgramThree {
+    public static void main(String[] args) {
+
+        int n1 = 10;
+        int n2 = 20;
+        int temp;
+        System.out.println("before swapping n1 = " + n1 + " and n2 = " + n2);
+
+        //swapping
+        temp = n1;
+        n1 = n2;
+        n2 = temp;
+        System.out.println("after swapping n1 = " + n1 + " and n2 = " + n2);
+		
+		System.out.println("Hello from past");
+    }
+}
